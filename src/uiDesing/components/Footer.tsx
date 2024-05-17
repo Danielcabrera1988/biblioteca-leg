551dea2662f82faca93f7b2571359fed094bf527
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2024-05-17 10:12:56 -0300

    add responsive font footer

--- src/uiDesing/components/Footer.tsx
+++ src/uiDesing/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="items-center min-w-full mt-auto text-center border">
-      <p className="m-auto text-[10px] text-center text-gray-400">
+    <footer className="flex items-center h-6 min-w-full mt-auto text-center border">
+      <p className="m-auto text-[10px] md:text-xs text-center text-gray-400">
         Desarrollo de Software: Poder Legislativo del Chaco - Jose M. Paz N°170
         - 1er. Piso. All Right Reserved 2024©.
       </p>
