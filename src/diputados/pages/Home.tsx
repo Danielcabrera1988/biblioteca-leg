d5bed7b9b09bdb6288cd287d7cf83611ae91412f
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2024-05-17 12:04:24 -0300

    fix home

--- src/diputados/pages/Home.tsx
+++ src/diputados/pages/Home.tsx
@@ -2,13 +2,13 @@ import { DipList } from "../components";
 
 export const Home = () => {
   return (
-    <div className="">
+    <>
       <h2 className="mx-auto mt-6 text-3xl text-center font-Avenir-Medium animate__animated animate__fadeIn ">
         Nómina actual del Poder Legislativo
       </h2>
       <div className="grid p-3 mt-2 place-items-center grid-cols-autofit animate__animated animate__fadeIn ">
         <DipList cantidad={8} />
       </div>
-    </div>
+    </>
   );
 };
