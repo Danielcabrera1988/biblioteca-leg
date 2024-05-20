f7b654513b30c77fd71646502f117159923932a2
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2024-05-20 15:29:52 -0300

    overwrite style navbarlink

--- src/uiDesing/components/NavBar.tsx
+++ src/uiDesing/components/NavBar.tsx
@@ -13,21 +13,21 @@ export const NavBar = () => {
       <Navbar.Collapse>
         <Navbar.Link
           as={Link}
-          className="w-16 px-2 py-1 transition duration-200 ease-in border-b-2 border-white md:text-center focus:text-cyan-500 hover:text-cyan-500 hover:border-cyan-500"
+          className="w-16 px-2 py-1 transition duration-200 ease-in border-b-2 border-white md:border-b-2 hover:bg-transparent md:text-center focus:text-cyan-500 hover:text-cyan-500 hover:border-cyan-500"
           to="/"
         >
           Home
         </Navbar.Link>
         <Navbar.Link
           as={Link}
-          className="w-20 px-2 py-1 transition duration-200 ease-in border-b-2 border-white md:text-center focus:text-cyan-500 hover:text-cyan-500 hover:border-cyan-500"
+          className="w-20 px-2 py-1 transition duration-200 ease-in border-b-2 border-white md:border-b-2 hover:bg-transparent md:text-center focus:text-cyan-500 hover:text-cyan-500 hover:border-cyan-500"
           to="/about"
         >
           Nosotros
         </Navbar.Link>
         <Navbar.Link
           as={Link}
-          className="w-20 px-2 py-1 transition duration-200 ease-in border-b-2 border-white md:text-center focus:text-cyan-500 hover:text-cyan-500 hover:border-cyan-500"
+          className="w-20 px-2 py-1 transition duration-200 ease-in border-b-2 border-white md:border-b-2 hover:bg-transparent md:text-center focus:text-cyan-500 hover:text-cyan-500 hover:border-cyan-500"
           to="/contact"
         >
           Contacto
