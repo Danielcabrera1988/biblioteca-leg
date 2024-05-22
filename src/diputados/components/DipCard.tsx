6123304b1c2cb897f50242dbf627d00b9dd22797
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2024-05-22 10:51:29 -0300

    fix style bgc carddip

--- src/diputados/components/DipCard.tsx
+++ src/diputados/components/DipCard.tsx
@@ -11,7 +11,7 @@ export const DipCard = ({ id, Nombre, Apellido, bloque, email }: DipMode) => {
   const dipImg = `/assets/diputados-img/${id}.png`;
 
   return (
-    <div className="flex place-content-between flex-col m-2 rounded-lg shadow-md h-[520px] w-[280px]">
+    <div className="flex place-content-between flex-col m-2 rounded-lg shadow-md h-[520px] w-[280px] bg-gray-100">
       <img
         className="object-cover rounded-t-lg"
         src={dipImg}
@@ -21,8 +21,12 @@ export const DipCard = ({ id, Nombre, Apellido, bloque, email }: DipMode) => {
         <h5 className="text-xl font-bold text-gray-800 font-Avenir-Medium ">
           {Nombre} {Apellido}
         </h5>
-        <p className="text-justify text-gray-500 font-Calibri">{bloque}</p>
-        <p className="text-justify text-gray-500 font-Calibri">{email}</p>
+        <p className="font-semibold text-justify text-gray-500 font-Calibri">
+          {bloque}
+        </p>
+        <p className="font-semibold text-justify text-gray-500 font-Calibri">
+          {email}
+        </p>
       </div>
       <Button url={`/dipPage/${id}`} name="Mas..." />
     </div>
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export const MainLayout = () => {
   return (
-    <main className="flex flex-col min-h-screen bg-slate-100">
+    <main className="flex flex-col min-h-screen bg-gray-50">
       <NavBar />
       <Outlet />
       <Footer />
